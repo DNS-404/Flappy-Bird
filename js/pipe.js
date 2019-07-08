a4e71e5b4eec7bb0668d4a3f79b046cf21fac1d2
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-07-08 16:45:38 +0530

    Create death logic and restart functionality

--- js/pipe.js
+++ js/pipe.js
@@ -1,18 +1,38 @@
 function Pipe() {
-  this.top = random(height/2);
-  this.bottom = random(height/2);
+  this.pos = 0;
+  this.top = 0;
+  this.bottom = 0;
+
   this.width = 10;
   this.x = width;
   this.y = 0;
   this.velocity = 2;
 
+  this.generateNewPipe = function(x) {
+    this.x = x;
+    this.pos = random(height/5, height-(height/5));
+    this.top = this.pos - random(60, 100);
+    this.bottom = this.pos + random(60, 100);
+    // print(this.top + " " + this.bottom);
+  }
+
   this.show = function() {
     fill(255);
     rect(this.x, 0, this.width, this.top);
-    rect(this.x, height-this.bottom, this.width, this.bottom);
+    rect(this.x, this.bottom, this.width, height-this.bottom);
   }
 
   this.move = function() {
     this.x -= this.velocity;
+  }
+
+  this.reachedEnd = function() {
+    return this.x < -this.width;
+  }
+
+  // Not at all accurate :p
+  this.collision = function(bird) {
+    return (bird.y < this.top || bird.y > this.bottom) &&
+          (bird.x > this.x && bird.x < this.x + this.width);
   }
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,23 +1,45 @@
 let bird;
-let pipe;
+let pipe1, pipe2;
+let gameplay;
 
 function setup() {
-  createCanvas(500, 600);
+  createCanvas(400, 600);
   bird = new Bird();
-  pipe = new Pipe();
+  pipe1 = new Pipe();
+  pipe1.generateNewPipe(width);
+  pipe2 = new Pipe();
+  pipe2.generateNewPipe(width+width/2);
+  gameplay = true;
 }
 
 function draw() {
   background(0);
-  bird.update();
+  if(gameplay){ bird.update(); }
   bird.show();
 
-  pipe.move();
-  pipe.show();
+  if(gameplay){ pipe1.move(); }
+  pipe1.show();
+  if(gameplay && pipe1.reachedEnd()){
+    pipe1.generateNewPipe(width);
+  }
+  if(gameplay && pipe1.collision(bird)){
+    gameplay = false;
+  }
+  if(gameplay){ pipe2.move(); }
+  pipe2.show();
+  if(gameplay && pipe2.reachedEnd()){
+    pipe2.generateNewPipe(width);
+  }
+  if(gameplay && pipe2.collision(bird)){
+    gameplay = false;
+  }
 }
 
 function keyPressed() {
   if(key == ' '){
     bird.fly();
+  }
+  if(keyCode == RETURN && !gameplay){
+    setup();
   }
 }
